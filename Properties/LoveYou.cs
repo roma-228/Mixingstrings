cfa68d8b2a2b7dfbcfa5362e8d703a8a07d7d05e
Roman <romandavydov@MacBook-Air-Roman.local>
2022-10-11 22:50:40 +0300

    commit Roma

---
+++ Properties/LoveYou.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Mixingstrings.Properties
+{
+    public class LoveYou
+    {
+        public LoveYou()
+        {
+            //I love Olha :)
+            //How do you like the idea of ​​going to Caesar on Wednesday?
+        }
+    }
+}
+
